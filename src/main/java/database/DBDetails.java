53ba248d5663a454b09d419e7dbdb9e8cc7111d4
Hari Prasath <63399698+Harimaran@users.noreply.github.com>
2022-07-08 17:31:00 +0530

    Update DBDetails.java

--- src/main/java/database/DBDetails.java
+++ src/main/java/database/DBDetails.java
@@ -3,7 +3,7 @@ package database;
 public class DBDetails {
     protected static final String serverName = "rds-qa-thadb.ccj7tvd1f9lb.us-east-1.rds.amazonaws.com";
     protected static final String port = "1433";
-    protected static final String user = "prowessqa";
-    protected static final String pass = "NewAvon@Prowess_qa";
-    protected static final String Name = "ACM";
+    protected static final String user = "prowessqa123";
+    protected static final String pass = "NewAvon@Prowess_qa1231";
+    protected static final String Name = "AMC";
 }
